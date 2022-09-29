a711786560a0cd6a773e6be8bc8c9f237439c1c3
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-09-30 02:42:11 +0900

    Maze

--- Week4/Week4/Maze(No.4).c
+++ Week4/Week4/Maze(No.4).c
@@ -40,9 +40,12 @@ int FindExit(int x, int y) {
         Maze[x][y] = PATH_COLOR;
         if(FindExit(x - 1, y) || FindExit(x, y + 1) || FindExit(x + 1, y) || FindExit(x, y - 1)) {
             return 1;
+        } else if(FindExit(x - 1, y) == 1 || FindExit(x, y + 1) == 1 || FindExit(x, y - 1) == 1) {
+            return 1;
+        } else {
+            Maze[x][y] = BLOCKED_COLOR;
+            return 1;
         }
-        Maze[x][y] = BLOCKED_COLOR;
-        return 0;
     }
 }
 
