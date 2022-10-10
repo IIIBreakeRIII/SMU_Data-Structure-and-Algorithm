40850ae838eeecde6f9144bb6404ca29be516dfe
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-10-10 16:44:30 +0900

    Linked List Sum

---
+++ Week6/Week6/LinkedListSum(No.3).c
@@ -0,0 +1,96 @@
+//
+//  LinkedListSum(No.3).c
+//  Week6
+//
+//  Created by 현소류 on 2022/10/10.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct ListType{
+    int size;
+    struct ListNode * head;
+    struct ListNode * tail;
+} ListType;
+
+typedef struct ListNode {
+    element data;
+    struct ListNode * link;
+} ListNode;
+
+ListType * create() {
+    ListType * list = (ListType *)malloc(sizeof(ListType));
+    list -> size = 0;
+    list -> head = list -> tail = NULL;
+    return list;
+}
+
+void Error(char * message) {
+    fprintf(stderr, "%s \n", message);
+    exit(1);
+}
+
+void InsertList(ListType * list, element item) {
+    ListNode * new = malloc(sizeof(ListNode));
+    if(new == NULL) {
+        Error("Fail To Assign Memory \n");
+    }
+    new -> data = item;
+    new -> link = NULL;
+    
+    if(list -> tail == NULL) {
+        list -> head = list -> tail = new;
+    }
+    else {
+        list -> tail -> link = new;
+        list -> tail = new;
+    }
+    list -> size++;
+}
+
+void PrintList(ListType * list) {
+    ListNode * p = list -> head;
+    for(int i = 0; i < list -> size - 1; i++) {
+        printf("%d -> ", p -> data);
+        p = p -> link;
+    }
+    printf("%d\n", p -> data);
+}
+
+int GetLength(ListType * list) {
+    return list -> size;
+}
+
+element Sum(ListType * list) {
+    ListNode * p = list -> head;
+    element sum = 0;
+    for(;p;p = p -> link) {
+        sum += p -> data;
+    }
+    return sum;
+}
+
+int main() {
+    ListType * list;
+    int n, data;
+    
+    list = create();
+    
+    printf("Enter The Number Of Node : ");
+    scanf("%d", &n);
+    printf("\n");
+    
+    for(int i = 0; i < n; i++) {
+        printf("Node #%d Data : ", i + 1);
+        scanf("%d", &data);
+        element item = data;
+        InsertList(list, item);
+    }
+    
+    printf("\n");
+    PrintList(list);
+    
+    printf("\nSum Of Data In Each Node : %d \n\n", Sum(list));
+}
