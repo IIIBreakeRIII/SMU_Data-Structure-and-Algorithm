9df31a7c320dbc18bdc79a2c9004d094fb64330b
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-11-14 16:32:09 +0900

    PrimFunction

---
+++ Week11/Week11/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Week11
+//
+//  Created by 현소류 on 2022/11/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
